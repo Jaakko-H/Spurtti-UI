1ff367eb289084c427d88d603f8fb3514fc97734
Jaakko-H <jaheikela@gmail.com>
2018-11-27 23:27:28 +0200

    Applied Angular Material and added view-records component.

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ViewRecordsComponent } from './view-records/view-records.component';
 
 export const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'view-records', component: ViewRecordsComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { MatGridListModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -8,16 +8,19 @@ import { HomeComponent } from './home/home.component';
 import { TopbarComponent } from './topbar/topbar.component';
 
 import { ROUTES } from './app-routing';
+import { ViewRecordsComponent } from './view-records/view-records.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TopbarComponent
+    TopbarComponent,
+    ViewRecordsComponent
   ],
   imports: [
     BrowserModule,
-    MatGridListModule,
+    MatButtonModule,
+    MatToolbarModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [],
