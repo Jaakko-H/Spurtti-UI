46fbc2447fce3857534a20d64f2ec86470fe6944
Jaakko-H <jaheikela@gmail.com>
2019-04-08 21:40:20 +0300

    Extended duration field in create-record view.

--- src/app/modules/create-record/pages/create-record.component.ts
+++ src/app/modules/create-record/pages/create-record.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ModuleWithComponentFactories, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { DistanceBasedRecord } from '@app/shared';
 import { DistanceBasedRecordApiService } from '@app/core';
-import * as _moment from 'moment';
+import * as _toMillis from 'ms';
 
 @Component({
   selector: 'app-create-record',
@@ -11,8 +11,10 @@ import * as _moment from 'moment';
 })
 export class CreateRecordComponent implements OnInit {
 
+  private TIME_UNIT_SEPARATOR = ':';
+
   public distance: number;
-  public duration: string;
+  public durationString: string;
   public entryTime: Date;
 
   public constructor(private distanceBasedRecordApiService: DistanceBasedRecordApiService) {}
@@ -20,7 +22,30 @@ export class CreateRecordComponent implements OnInit {
   public ngOnInit() {}
 
   public async onSubmit(): Promise<void> {
-    await this.distanceBasedRecordApiService.createRecord(
-        new DistanceBasedRecord('1', 'Run', this.entryTime, this.distance, 1));
+    const duration = this.validateDuration();
+    if (isNaN(duration)) {
+      // TODO
+    } else {
+      await this.distanceBasedRecordApiService.createRecord(
+        new DistanceBasedRecord('1', 'Run', this.entryTime, this.distance, duration));
+    }
+  }
+
+  private validateDuration(): number {
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    const parts = this.durationString.split(this.TIME_UNIT_SEPARATOR, 3);
+    if (parts.length === 3) {
+      hours = Number(parts[0]);
+      minutes = Number(parts[1]);
+      seconds = Number(parts[2]);
+    } else if (parts.length === 2) {
+      minutes = Number(parts[0]);
+      seconds = Number(parts[1]);
+    } else {
+      seconds = Number(parts[0]);
+    }
+    return _toMillis(hours + 'h') + _toMillis(minutes + 'm') + _toMillis(seconds + 's');
   }
 }
